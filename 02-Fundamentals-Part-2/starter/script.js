bf252497010e54095de948a2c0416dfae6b30f6b
unknown <you@example.com>
2024-05-25 18:02:53 +0800

    Object BMI practice

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,27 +1,27 @@
 'use strict';
-const jonas = {
-    firstName: 'Jonas',
-    lastName: 'Schedtmann',
-    birthYear: 1991,
-    job: 'teacher',
-    friends: ['Michael', 'Peter', 'Steven'],
-    hasDriversLicence: false,
+// const jonas = {
+//     firstName: 'Jonas',
+//     lastName: 'Schedtmann',
+//     birthYear: 1991,
+//     job: 'teacher',
+//     friends: ['Michael', 'Peter', 'Steven'],
+//     hasDriversLicence: false,
 
-    calcAge: function () {
-        this.age = 2037 - this.birthYear;
-        return this.age;
-    },
+//     calcAge: function () {
+//         this.age = 2037 - this.birthYear;
+//         return this.age;
+//     },
 
-    getSummary: function () {
-        return `${this.firstName} is a ${this.calcAge()}-year old ${this.job}, and he has ${this.hasDriversLicence ? 'a' : 'no'} driver's licence. `
-    },
+//     getSummary: function () {
+//         return `${this.firstName} is a ${this.calcAge()}-year old ${this.job}, and he has ${this.hasDriversLicence ? 'a' : 'no'} driver's licence. `
+//     },
 
 
-};
+// };
 
-console.log(jonas.calcAge());
-console.log(jonas.age);
-console.log(jonas.getSummary());
+// console.log(jonas.calcAge());
+// console.log(jonas.age);
+// console.log(jonas.getSummary());
 
 
 // console.log(jonas['lastName']);
@@ -43,3 +43,32 @@ console.log(jonas.getSummary());
 // jonas['twitter'] = '@jonasschmedtman';
 // console.log(`${jonas.firstName} has ${jonas.friends.length} firends, and his best friend is called ${jonas.friends[0]}`);
 
+const mark = {
+    fullName: "Mark Miller",
+    mass: 78,
+    height: 1.69,
+    calcBMI: function () {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    }
+};
+
+const john = {
+    fullName: "John Smith",
+    mass: 92,
+    height: 1.95,
+    calcBMI: function () {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    }
+};
+
+mark.calcBMI();
+john.calcBMI();
+
+
+if (mark.bmi > john.bmi) {
+    console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})`);
+} else if (john.bmi > mark.bmi) {
+    console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})`);
+}
